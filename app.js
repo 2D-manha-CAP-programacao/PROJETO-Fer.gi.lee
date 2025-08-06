b2417dae5297e950e48aa541528caffd595e20e3
fer.gi.lee <giovana.cerqueira.teixeira@escola.pr.gov.br>
2025-08-06 11:53:04 +0000

    aula31-pag9

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 function criarCartao(categoria, pergunta, resposta) {
-    console.log(categoria, pergunta, resposta);
-
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+    cartao.innerHTML = '
+    <div class="cartao_conteudo">
+    <h3>título mudar</h3>
+    <div class="cartao_conteudo_pergunta">
+        <p>Pergunta</p>
+    </div> 
+    <div class="cartao_conteudo_resposta">
+        <p>Resposta</p>
+    </div>
+    </div>
+    '
 }
